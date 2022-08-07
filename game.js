3faec944c90fd031b15529c237cd8ff6a3f47c20
Leah Young <leah.young21@alumni.colostate.edu>
2022-08-07 16:15:49 -0600

    add checkForWin, win, detectDraw functions

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ class Game {
     this.playerTwo = new Player('two', 'o');
     this.gameBoard = ["", "", "", "", "", "", "", "", ""];
     this.startingPlayer = this.playerOne;
+    this.activePlayer = this.startingPlayer;
   }
 
-  changeTurns() {
+  setStartingPlayer() {
     if (this.startingPlayer === this.playerOne) {
       this.startingPlayer = this.playerTwo
     } else if (this.startingPlayer === this.playerTwo) {
@@ -14,81 +15,82 @@ class Game {
     }
   }
 
-  trackBoardData(anyIndex) {
-    if (this.gameBoard[anyIndex] === "") {
-      if (this.startingPlayer === this.playerOne) {
-        this.gameBoard[anyIndex] = this.playerOne.token
-        this.changeTurns();
-      } else if (this.startingPlayer === this.playerTwo) {
-        this.gameBoard[anyIndex] = this.playerTwo.token
-        this.changeTurns();
-      }
+  updateActivePlayer() {
+    if (this.activePlayer === this.playerOne) {
+      this.activePlayer = this.playerTwo
+    } else {
+      this.activePlayer = this.playerOne
     }
-    this.trackWins();
   }
 
+  trackBoardData(anyIndex) {
+    if (this.gameBoard[anyIndex] === "") {
+      if (this.activePlayer === this.playerOne) {
+        this.gameBoard[anyIndex] = this.playerOne.token
+      }
+      if (this.activePlayer === this.playerTwo) {
+        this.gameBoard[anyIndex] = this.playerTwo.token
+      }
+    }
+    this.checkForWin();
+    this.updateActivePlayer();
+  }
 
-  trackWins() {
+  checkForWin() {
     // horizontal wins
-    if (this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2] && this.gameBoard[2] === this.gameBoard[0] !== "") {
-      console.log("winner!")
+    if (this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2] && this.gameBoard[2] !== "") {
+      this.win();
     }
-    else if (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[3] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5] && this.gameBoard[5] !== "") {
+      this.win();
     }
-    else if (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8] && this.gameBoard[8] === this.gameBoard[6] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8] && this.gameBoard[8] !== "") {
+      this.win();
     }
     // vertical wins
-    else if (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6] && this.gameBoard[6] === this.gameBoard[0] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6] && this.gameBoard[6] !== "") {
+      this.win();
     }
-    else if (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[1] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7] && this.gameBoard[7] !== "") {
+      this.win();
     }
-    else if (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8] && this.gameBoard[8] === this.gameBoard[2] !== "") {
-      console.log("winner!")
-    }
+    else if (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8] && this.gameBoard[8] !== "") {
+      this.win();    }
     // diagonal wins
-    else if (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6] && this.gameBoard[6] === this.gameBoard[2] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6] && this.gameBoard[6] !== "") {
+      this.win();
     }
-    else if (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8] && this.gameBoard[8] === this.gameBoard[0] !== "") {
-      console.log("winner!")
+    else if (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8] && this.gameBoard[8] !== "") {
+      this.win();
     } else {
       this.detectDraw();
     }
   }
 
-  displayWinner() {
-
+  win() {
+    if (this.activePlayer === this.playerOne) {
+      this.playerOne.increaseWins();
+      this.setStartingPlayer();
+    }
+    if (this.activePlayer === this.playerTwo) {
+      this.playerTwo.increaseWins();
+      this.setStartingPlayer();
+    }
   }
 
-  detectDraw(indexes) {
-    if (this.gameBoard[indexes] !== "" ) {
+  detectDraw() {
+    if (!this.gameBoard.includes("")) {
       console.log("draw!")
     }
-
   }
 
-  // resetGrid() {
-  //   // set a conditional so that once a win is declared,
-  //   // the game board resets, need to call on the trackWins() fxn
-  //   // to update scores and tally, then clear out (change innerHTML)
-  // }
+  resetGrid() {
+    this.gameBoard = ["", "", "", "", "", "", "", "", ""];]
+
+    // set a conditional so that once a win is declared,
+    // the game board resets, need to call on the trackWins() fxn
+  }
 }
 
-// A way to check the Game’s board data for win conditions
-// A way to detect when a game is a draw (no one has won)
+
 // A way to reset the Game’s board to begin a new game
-
-
-
-
-
-
-
-
-
-// Important aspects of the game "Tic Tac Toe":
-    //
