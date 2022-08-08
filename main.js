d0c67acb79f2644c47de7771607d65d95ff8d8ac
Leah Young <leah.young21@alumni.colostate.edu>
2022-08-07 20:15:05 -0600

    add querySelectors, event listeners and playTheGame function

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 // QUERYSELECTORS //
 var gameGrid = document.querySelector('.main-game-container')
-var
+var displayPlayerTurn = document.querySelector('.players-turn')
+var firstPlayerScore = document.querySelector('.player-one-score')
+var secondPlayerScore = document. querySelector('.player-two-score')
+var gameGridSpaces = document.querySelectorAll('.spaces')
 
-
-// EVENT LISTENERS & FUNCTIONS //
+// EVENT LISTENERS //
+gameGrid.addEventListener('click', playTheGame)
 
 // GLOBAL VARIABLE //
 var ticTacToe = new Game();
+
+ // FUNCTIONS //
+
+playTheGame() {
+  for (var i = 0; i < gameGridSpaces[i].length; i++) {
+    if (gameGridSpaces[i].id === event.target.id && ticTacToe.activePlayer === ticTacToe.playerOne &&
+      gameGridSpaces.innerHTML = "") {
+        gameGridSpaces[i].innerHTML = `
+        <h1>${ticTacToe.playerOne.token}</h1>
+        `
+    }
+    // will need multiple conditions
+  }
+}
