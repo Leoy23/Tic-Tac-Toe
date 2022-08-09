a03754349434e9e03163b6bc060673741464b3ba
Leah Young <leah.young21@alumni.colostate.edu>
2022-08-09 14:11:45 -0600

    remove hide function

--- main.js
+++ main.js
@@ -78,11 +78,6 @@ function timer() {
   }, 2000);
 }
 
-
-function hide(element) {
-    element.classList.add('hidden');
-  }
-
   function show(element) {
     element.classList.remove('hidden');
   }
